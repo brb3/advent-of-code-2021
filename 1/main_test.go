902fc80391af8772b4879b38d2bbedc2b5373c9a
Bobby Burden III <bobby.burden@roushtech.net>
2021-12-02 20:28:58 -0500

    Add tests for day 1. Shamelessly stolen from t-richards

---
+++ 1/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPart1(t *testing.T) {
+	input := []int{
+		199,
+		200,
+		208,
+		210,
+		200,
+		207,
+		240,
+		269,
+		260,
+		263,
+	}
+
+	actual := partOne(input)
+
+	assert.Equal(t, 7, actual)
+}
+
+func TestPart2(t *testing.T) {
+	input := []int{
+		199,
+		200,
+		208,
+		210,
+		200,
+		207,
+		240,
+		269,
+		260,
+		263,
+	}
+
+	actual := partTwo(input)
+
+	assert.Equal(t, 5, actual)
+}
