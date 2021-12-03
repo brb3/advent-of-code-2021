9b12f941fbbba73381dc50932abcb1efb24d0b3e
Bobby Burden III <bobby.burden@roushtech.net>
2021-12-02 21:24:28 -0500

    day 2, part 2

--- advent/2/02.go
+++ advent/2/02.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	lines := advent_utils.LoadData("input.txt")
 	fmt.Println("Part One:", partOne(lines))
+	fmt.Println("Part One:", partTwo(lines))
 }
 
 func partOne(input []string) int {
@@ -26,6 +27,28 @@ func partOne(input []string) int {
 			depth += int(value)
 		case "up":
 			depth -= int(value)
+		}
+	}
+
+	return horizontal * depth
+}
+
+func partTwo(input []string) int {
+	horizontal := 0
+	depth := 0
+	aim := 0
+
+	for i := 0; i < len(input); i++ {
+		split := strings.Split(input[i], " ")
+		value, _ := strconv.ParseInt(split[1], 10, 0)
+		switch split[0] {
+		case "forward":
+			horizontal += int(value)
+			depth += aim * int(value)
+		case "down":
+			aim += int(value)
+		case "up":
+			aim -= int(value)
 		}
 	}
 
--- advent/2/02_test.go
+++ advent/2/02_test.go
@@ -20,3 +20,18 @@ func TestPartOne(t *testing.T) {
 
 	assert.Equal(t, 150, actual)
 }
+
+func TestPartTwo(t *testing.T) {
+	input := []string{
+		"forward 5",
+		"down 5",
+		"forward 8",
+		"up 3",
+		"down 8",
+		"forward 2",
+	}
+
+	actual := partTwo(input)
+
+	assert.Equal(t, 900, actual)
+}
